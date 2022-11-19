8cf205b46bb82b654537e7c9630442c119f5c28b
Hannick <abdul.hannick@gmail.com>
2022-11-19 13:52:13 +0100

    objets js et php

--- projet/objets.php
+++ projet/objets.php
@@ -1,13 +1,14 @@
 <?php
-    $link = mysqli_connect('localhost', 'root', 'root', 'geobase');
+    $link = mysqli_connect('localhost', 'root', 'root', 'projet_web_db');
     mysqli_set_charset($link, "utf8");
 ?>
 
 <?php
-    $results = mysqli_query($link, "SELECT * FROM objets");
+    $id = $_GET["id"];
+    $results = mysqli_query($link, "SELECT * FROM objets WHERE id = $id");
     $array = [];
     foreach($results as $result){
         array_push($array,$result);
     }
-    echo json_encode($array)
+    echo json_encode($array,JSON_NUMERIC_CHECK)
 ?>
