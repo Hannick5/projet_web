bc3879215f4e7a0558fe5bfa6246eb9194e333e0
AxelDmt <jagertykah@gmail.com>
2022-11-20 13:05:18 +0100

    dqsfc

--- projet/forms/register/register.js
+++ projet/forms/register/register.js
@@ -1,56 +1,62 @@
-  const username = document.getElementById('username')
-        const password = document.getElementById('password')
-        const check = document.getElementById('check')
-        const spinner = document.getElementById('spinner')
-        const button = document.getElementById('connect')
-        const cpass = document.getElementById("confirmpassword")
-        var username_valid = false;
-        var password_valid = false;
-        var cpass_valid = false;
-
-        username.oninput = function() {
-            if (this.value.length > 4) {
-                username_valid = true
-            } else {
-                username_valid = false
-            }
-            form_valid(username_valid, password_valid,cpass_valid)
-        }
-
-        password.oninput = function() {
-            if (this.value.length > 8) {
-                password_valid = true
-            } else {
-                password_valid = false
-            }
-
-            form_valid(username_valid, password_valid,cpass_valid)
-        }
-        cpass.oninput = function(){
-            if (this.value==password.value) {
-                cpass_valid = true
-            } else {
-                cpass_valid = false
-            }
-
-            form_valid(username_valid, password_valid,cpass_valid)
-        }
-
-        function form_valid(username_valid, password_valid) {
-            if (username_valid && password_valid && cpass_valid) {
-                spinner.style.display = 'none'
-                check.style.display = 'block'
-
-                button.classList.add('valid')
-                check.classList.add('up')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const check = document.getElementById('check')
+const spinner = document.getElementById('spinner')
+const button = document.getElementById('connect')
+const cpass = document.getElementById("confirmpassword")
+var username_valid = false;
+var password_valid = false;
+var cpass_valid = false;
 
 
-            } else {
-                spinner.style.display = 'block'
-                check.style.display = 'none'
-                button.classList.remove('valid')
+username.oninput = function() {
+    if (this.value.length > 4) {
+        username_valid = true
+    } else {
+        username_valid = false
+    }
+    form_valid(username_valid, password_valid,cpass_valid)
+}
 
-                check.classList.remove('up')
+password.oninput = function() {
+    if (this.value.length > 8) {
+        password_valid = true
+    } else {
+        password_valid = false
+    }
 
-            }
-        }
+    form_valid(username_valid, password_valid,cpass_valid)
+}
+cpass.oninput = function(){
+    if (this.value==password.value) {
+        cpass_valid = true
+    } else {
+        cpass_valid = false
+    }
+
+    form_valid(username_valid, password_valid,cpass_valid)
+}
+
+function form_valid(username_valid, password_valid) {
+    if (username_valid && password_valid && cpass_valid) {
+        spinner.style.display = 'none'
+        check.style.display = 'block'
+
+        button.classList.add('valid')
+        check.classList.add('up')
+        
+
+
+    } else {
+        spinner.style.display = 'block'
+        check.style.display = 'none'
+        button.classList.remove('valid')
+
+        check.classList.remove('up')
+
+    }
+}
+
+
+
+
